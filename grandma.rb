881231120e5e7503ad64549d5e07a093485e8a8e
Scott Rodriguez <scottmrodriguez51@gmail.com>
2020-08-01 13:22:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,6 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-    end
     elsif phrase != phrase.upcase()
       return "HUH?! SPEAK UP, SONNY!"
   else
